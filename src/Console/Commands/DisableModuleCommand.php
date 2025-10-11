9324ba5f38c04bb139a205672ee78f2a457d8b7d
Aure Dulvresse <adentrepreneur02@gmail.com>
2025-10-11 03:02:43 +0100

    refacto (console): Rename and refacto Commands

---
+++ src/Console/Commands/DisableModuleCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IronFlow\Console\Commands;
+
+use Illuminate\Console\Command;
+use IronFlow\Facades\Anvil;
+
+/**
+ * DisableModuleCommand
+ */
+class DisableModuleCommand extends Command
+{
+    protected $signature = 'ironflow:module:disable {name : The name of the module}';
+    protected $description = 'Disable an IronFlow module';
+
+    public function handle(): int
+    {
+        $name = $this->argument('name');
+
+        try {
+            Anvil::disable($name);
+            $this->info("Module {$name} disabled successfully!");
+            return 0;
+        } catch (\Exception $e) {
+            $this->error("Failed to disable module: {$e->getMessage()}");
+            return 1;
+        }
+    }
+}
---
+++ src/Console/Commands/EnableModuleCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IronFlow\Console\Commands;
+
+use Illuminate\Console\Command;
+use IronFlow\Facades\Anvil;
+
+/**
+ * EnableModuleCommand
+ */
+class EnableModuleCommand extends Command
+{
+    protected $signature = 'ironflow:module:enable {name : The name of the module}';
+    protected $description = 'Enable an IronFlow module';
+
+    public function handle(): int
+    {
+        $name = $this->argument('name');
+
+        try {
+            Anvil::enable($name);
+            $this->info("Module {$name} enabled successfully!");
+            return 0;
+        } catch (\Exception $e) {
+            $this->error("Failed to enable module: {$e->getMessage()}");
+            return 1;
+        }
+    }
+}
