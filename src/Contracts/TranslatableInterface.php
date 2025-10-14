65eb9057e599efa75ea5e45891f96d41844ff7b0
Aure Dulvresse <adentrepreneur02@gmail.com>
2025-10-14 14:37:56 +0100

    feat: Add TranslatableInterface for allow module to provide translations

---
+++ src/Contracts/TranslatableInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IronFlow\Contracts;
+
+
+/**
+ * TranslatableInterface
+ *
+ * Allows module to provide translations.
+ */
+interface TranslatableInterface
+{
+    /**
+     * Get the translation path for this module.
+     *
+     * @return string
+     */
+    public function getTranslationPath(): string;
+
+    /**
+     * Get translation namespace.
+     *
+     * @return string
+     */
+    public function getTranslationNamespace(): string;
+}
